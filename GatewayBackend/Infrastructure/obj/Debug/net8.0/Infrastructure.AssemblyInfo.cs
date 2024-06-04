e1dfa51f753792c7630dd7fdb4afea69fbb843c3
Daniel <danieladrianzen@gmail.com>
2024-06-04 00:13:15 -0400

    working backend

--- GatewayBackend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ GatewayBackend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15405972f6996c55fa570befa6fc695a33d1fefb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ef8fde64aa054c5ba5f1c1951848348a11b3a09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
