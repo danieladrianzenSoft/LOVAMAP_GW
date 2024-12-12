d879694ded1c6c450c9e80b436e6e22c18a54ec0
Daniel <danieladrianzen@gmail.com>
2024-12-12 18:12:01 -0500

    attempt at deployment

--- GatewayBackend/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ GatewayBackend/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f32e910ad3c819dd3cb3d8f40c2b70dd90e4bd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1651550fb1904546b09a0878750eeafdb39ff50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
