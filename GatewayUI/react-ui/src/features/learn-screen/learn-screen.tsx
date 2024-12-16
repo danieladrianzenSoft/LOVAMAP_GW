78a561df8bc707a6e66f8b9cc3e2d5df16a7015a
Daniel <danieladrianzen@gmail.com>
2024-12-16 03:21:04 -0500

    sorted descriptor types

--- GatewayUI/react-ui/src/features/learn-screen/learn-screen.tsx
+++ GatewayUI/react-ui/src/features/learn-screen/learn-screen.tsx
@@ -70,7 +70,10 @@ const LearnScreen = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {groupedDescriptorTypes[activeTab].map((descriptor, index) => (
+                            {groupedDescriptorTypes[activeTab]
+                                .slice()
+                                .sort((a, b) => a.name.localeCompare(b.name))
+                                .map((descriptor, index) => (
                                 <tr key={index} className="border-t border-gray-300 last:border-b">
                                     <td className="px-4 py-2 border-l border-gray-300 whitespace-normal" style={{ width: "15%" }}>
                                         <div className="max-w-[150px]">{descriptor.tableLabel}</div>
