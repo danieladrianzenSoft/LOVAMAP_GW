2736e8fcef4d78180c74c0af46e05e47f371dfa5
Daniel <danieladrianzen@gmail.com>
2025-03-09 17:52:37 -0400

    just making sure

--- GatewayBackend/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ GatewayBackend/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81fdf435420f22da0a213c76e8804d1f33dcc013")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74f956fae0d2ab069836c088c4f756cb1f5698d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
