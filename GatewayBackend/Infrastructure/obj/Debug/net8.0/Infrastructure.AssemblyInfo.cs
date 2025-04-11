d113a77fc05966ae112810d31752d6b57415ab5c
Daniel <danieladrianzen@gmail.com>
2025-04-11 13:18:29 -0400

    latest stuff

--- GatewayBackend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ GatewayBackend/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b05440c3e777f073fe8db038b0a4e9a21b43734e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a28cc258aa377f2872446b036c39b0b8fd95d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
